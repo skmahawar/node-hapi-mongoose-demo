371da3f3427dfc9a8ff9bb2d0caf5a8ef654d317
Suresh Mahawar <suresh.mahawar@hngre.com>
2016-01-25 19:24:27 +0530

    scoail

--- services/social.js
+++ services/social.js
@@ -6,8 +6,51 @@
     var request = require('request');
     var moment = require('moment');
     var Async = require('async');
+    var google = require('googleapis');
+    var plus = google.plus('v1');
+    var oauth2Client = new OAuth2(CLIENT_ID, CLIENT_SECRET, REDIRECT_URL);
     var winston = require('../winston');
     var config = require('../config/config');
+
+
+    exports.refreshToken = function(type, accessToken) {
+        switch (type) {
+            case 'facebook':
+                {
+                    var url = 'https://graph.facebook.com/oauth/access_token?client_id=APP_ID&client_secret=APP_SECRET&grant_type=fb_exchange_token&fb_exchange_token=EXISTING_ACCESS_TOKEN';
+                    request(url, function(err, response, body) {
+
+                        if (err || response.statusCode !== 200) {
+                            return callback(err);
+                        } else {
+                            try {
+                                body = JSON.parse(body);
+                            } catch (err) {
+                                return callback(err);
+                            }
+                            winston.info(body);
+                            var user = _.pick(body, 'first_name', 'last_name', 'gender', 'birthday', 'email');
+                            user.facebook = body;
+                            user.provider = 'facebook';
+                            if (user.email) {
+                                user.email_verified = true;
+                            }
+                            if (user.birthday) {
+                                user.birthday = moment(new Date(user.birthday));
+                            }
+
+                            callback(null, user);
+                        }
+                    });
+                };
+                break;
+            case 'google':
+                {
+
+                };
+                break;
+        }
+    }
 
     /**
      * [Login with Facebook description]
@@ -51,28 +94,19 @@
      */
     exports.google = function(payload, callback) {
         var url = 'https://graph.facebook.com/me?access_token=' + payload.access_token;
-        request(url, function(err, response, body) {
-
-            if (err || response.statusCode !== 200) {
+        var OAuth2 = google.auth.OAuth2;
+        oauth2Client.getToken(payload.access_token, function(err, tokens) {
+            // Now tokens contains an access_token and an optional refresh_token. Save them.
+            if (err) {
                 return callback(err);
-            } else {
-                try {
-                    body = JSON.parse(body);
-                } catch (err) {
-                    return callback(err);
-                }
-                winston.info(body);
-                var user = _.pick(body, 'first_name', 'last_name', 'gender', 'birthday', 'email');
-                user.facebook = body;
-                if (user.email) {
-                    user.email_verified = true;
-                }
-                if (user.birthday) {
-                    user.birthday = moment(new Date(user.birthday));
-                }
-
-                callback(null, user);
             }
+            oauth2Client.setCredentials(tokens);
+            plus.people.get({
+                userId: 'me',
+                auth: oauth2Client
+            }, function(err, response) {
+                // handle err and response
+            });
         });
     };
 
@@ -149,6 +183,6 @@
      * @return {[type]}            [description]
      */
     exports.custom = function(payload, callback) {
-    	return payload;
+        return payload;
     }
 }())
